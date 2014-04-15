651444f8e6103bb009f45b8b3cfc35679858097f
Art Chaidarun <art@duolingo.com>
2014-04-15 16:55:04 -0400

    Update database settings

--- configheroku.py
+++ configheroku.py
@@ -1,5 +1,5 @@
 SECRET_KEY = "\xd37;+gj\n\xf4E\xfe\xbe\xd1\xb2Z\x1d\xd4\x00\x16{\x162\xd3\xfd\xb8"
 
-USERNAME = "gydbgppgygktry"
-PASSWORD = "WiXl1gm7OxmGWPR9OuC1XWzR5b"
-SQLALCHEMY_DATABASE_URI = "postgres://" + USERNAME + ":" + PASSWORD + "@ec2-54-204-44-31.compute-1.amazonaws.com:5432/da2gnsolcbplt1"
+USERNAME = "ciyvnupxjpavna"
+PASSWORD = "8QHpY-lEPfB8SGsdPrrFuCFnFh"
+SQLALCHEMY_DATABASE_URI = "postgres://" + USERNAME + ":" + PASSWORD + "@ec2-54-204-16-232.compute-1.amazonaws.com:5432/d1bpe6imctuoln"
